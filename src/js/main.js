a51dc8229cec70acdb422668867dae9eefd8d18b
AQ <124173634+aqadir12@users.noreply.github.com>
2024-05-13 17:06:58 +0500

    git init v1.0

--- src/js/main.js
+++ src/js/main.js
@@ -6,4 +6,6 @@ import * as bootstrap from 'bootstrap'; // eslint-disable-line no-unused-vars
 
 import './_color_mode_picker';
 
+
+
 // import '../../node_modules/bootstrap-icons/font/bootstrap-icons.scss';
